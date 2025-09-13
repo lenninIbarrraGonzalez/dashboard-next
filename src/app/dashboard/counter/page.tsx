12b0184aac448cf684a4d330c1da6228594de02c
Lenin Ibarra <ing.lenninibarra@gmail.com>
2025-09-13 15:19:45 -0500

    pensando como implementar hojas en server components, division de un use client

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,28 +1,14 @@
-"use client";
-import { useState } from "react";
+import { CartCounter } from "../../shopping-cart/components/CartCounter";
+export const metadata = {
+  title: "Counter Page",
+  description: "This is the counter page",
+};
 
 export default function CounterPage() {
-  const [count, setCount] = useState(5);
-
   return (
     <div className="flex flex-col items-center justify-center h-full w-full">
       <span>Productos en el carrito</span>
-      <span className="text-9xl">{count}</span>
-
-      <div className="flex mt-5">
-        <button
-          className="flex items-center justify-center p-2 rounder-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100] mr-2"
-          onClick={() => setCount(count + 1)}
-        >
-          +1
-        </button>
-        <button
-          className="flex items-center justify-center p-2 rounder-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100] mr-2"
-          onClick={() => setCount(count - 1)}
-        >
-          -1
-        </button>
-      </div>
+      <CartCounter value={20} />
     </div>
   );
 }
---
+++ src/app/shopping-cart/components/CartCounter.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useState } from "react";
+
+interface Props {
+  value?: number;
+}
+
+export const CartCounter = ({ value }: Props) => {
+  const [count, setCount] = useState(value || 0);
+  return (
+    <>
+      <span className="text-9xl">{count}</span>
+
+      <div className="flex mt-5">
+        <button
+          className="flex items-center justify-center p-2 rounder-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100] mr-2"
+          onClick={() => setCount(count + 1)}
+        >
+          +1
+        </button>
+        <button
+          className="flex items-center justify-center p-2 rounder-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100] mr-2"
+          onClick={() => setCount(count - 1)}
+        >
+          -1
+        </button>
+      </div>
+    </>
+  );
+};
